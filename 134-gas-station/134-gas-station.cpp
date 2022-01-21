7941a30ddbdc63cfe4b02f9aaed6e02248c8b41e
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-01-22 02:13:39 +0530

    Time: 124 ms (11.72%), Space: 49.8 MB (45.58%) - LeetHub

--- 134-gas-station/134-gas-station.cpp
+++ 134-gas-station/134-gas-station.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int len=gas.size(),total_difference=0,current_difference=0,start=0;
-        for(int i=0, j=0;i<len;i++){
-            int difference=gas[i]-cost[i];
-            total_difference+=difference;
-            current_difference+=difference;
-            if( current_difference<0  ) {//if my fuel got exhausted
-                start=i+1;
-                current_difference=0;
+        int gas_all =0,cost_all=0,len=gas.size();
+        for(int i=0;i<len;i++) gas_all+=gas[i],cost_all+=cost[i];
+        if(gas_all < cost_all)
+            return -1;
+        //this part will be called only if there exists an idx
+        int total_value = 0;
+        int start_index = 0;
+        for(int i=0;i<cost.size();i++){
+            total_value += gas[i]-cost[i];
+            if(total_value<0){
+                start_index = i+1;
+                total_value = 0;
             }
         }
         
-        return (total_difference<0)?-1:start;
+        return start_index;
+        
     }
 };
