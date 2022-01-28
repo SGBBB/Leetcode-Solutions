9ddf87e4ddb0169c6f0bfae1a63feab5f2ffc4f0
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-01-29 00:57:16 +0530

    Time: 116 ms (97.18%), Space: 93.2 MB (37.71%) - LeetHub

--- 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     long long getDescentPeriods(vector<int>& prices) {
         long long i=0,len=prices.size(),ans=0;
-        for(;i<len;i++){
+        for(;i<len;){
             long long cnt=1;
-            for(;i+1 <len and prices[i+1]==prices[i]-1;cnt++,i++){ }
+            i++;
+            for(;i <len and prices[i]==prices[i-1]-1;cnt++,i++){ }
             ans+=( (cnt*(cnt+1))/2);
         }
         return ans;
