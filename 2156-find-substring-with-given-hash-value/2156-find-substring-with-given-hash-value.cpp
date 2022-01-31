dd15524769c7e8577c05a44f694ea698e5ba2235
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-01-31 12:50:00 +0530

    Time: 542 ms (5.88%), Space: 10.3 MB (11.76%) - LeetHub

--- 2156-find-substring-with-given-hash-value/2156-find-substring-with-given-hash-value.cpp
+++ 2156-find-substring-with-given-hash-value/2156-find-substring-with-given-hash-value.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     string subStrHash(string s, int power, int mod, int k, int hashValue) {
         long long hash = 0, res = 0, power_k = 1;
-    auto val = [&](int i){ return s[i] - '`'; };
+    
         long long i = s.size() - 1,j=i,K=k;
     for(;K--;i--){
-        hash = (hash * power + val(i)) % mod;
+        hash = (hash * power +(s[i]-'a'+1)) % mod;
         power_k = power_k * power % mod;
         if (hash == hashValue)
             { res=i;cout<<"hash is " << endl;   }
@@ -13,8 +13,8 @@ public:
     
     
     for (; i >= 0; --i,--j) {
-        hash = (hash * power + val(i)) % mod;
-        hash = (mod + hash - (power_k * val(j) ) % mod) % mod;
+        hash = (hash * power + (s[i]-'a'+1)) % mod;
+        hash = (mod + hash - (power_k * (s[j]-'a'+1) ) % mod) % mod;
         
         cout<<"hash is " <<hash<<endl;    
         if (hash == hashValue)
