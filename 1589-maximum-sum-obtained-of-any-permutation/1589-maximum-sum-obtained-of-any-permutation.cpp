134c220e7b94cb6ef56939520277ec322cf68a07
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-02-05 01:56:03 +0530

    Time: 693 ms (29.51%), Space: 135.5 MB (17.38%) - LeetHub

---
+++ 1589-maximum-sum-obtained-of-any-permutation/1589-maximum-sum-obtained-of-any-permutation.cpp
@@ -0,0 +1,26 @@
+bool cmp(long long a,long long b){
+    return a>b;
+}
+
+class Solution {
+    int mod=1e9 +7;
+public:
+    int maxSumRangeQuery(vector<int>& nums, vector<vector<int>>& requests) {
+        long long n=nums.size(),i=0,mod=1e9+7;
+        vector<long long> cnt(n);
+        for(auto j:requests) {
+            cnt[j[0]]++;
+            if(j[1]<n-1) cnt[j[1]+1]--; 
+        }
+        for(i=1;i<n;i++){
+            cnt[i]+=cnt[i-1];
+        }
+        sort(cnt.begin(),cnt.end(),cmp);
+        sort(nums.begin(),nums.end() ,cmp);
+        long long sum=0;
+        for(i=0;i<n;i++){
+            sum+=((nums[i]) * (cnt[i]));
+        }
+        return sum%mod;
+    }
+};
