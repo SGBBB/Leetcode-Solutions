1212c82f00607716cad846231de20f8a18a335eb
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-02-08 11:46:47 +0530

    Time: 5 ms (17.94%), Space: 6 MB (25.37%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     int addDigits(int num) {
-        if(num/10){
-            int temp=num,sum=0;
-            for(;temp;sum+=(temp%10), temp/=10){}
-            return addDigits(sum);
-            
-        }
-        return num;
+        return (!num)?0 : !(num%9)? 9: num%9;  
     }
 };
