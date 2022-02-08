369c6d71348d2db002f0e4ea6f42832e4d8d765c
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-02-08 23:30:07 +0530

    Time: 219 ms (15.32%), Space: 54 MB (20.67%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -10,7 +10,7 @@ public:
                 pq.push(diff);
             bricks-=diff;
             if(bricks<0){ //if bricks are 0 this means we have already used  bricks
-                ladders--;
+                ladders--; //if im short of bricks then use ladders
                 
                 bricks+=pq.top(), pq.pop();
                 if(ladders<0 or bricks<0) return i-1;
