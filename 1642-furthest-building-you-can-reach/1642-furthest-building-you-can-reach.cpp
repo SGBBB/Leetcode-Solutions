3696a4eac10b8c69d472cd9323b6f784c9fafae0
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-02-08 23:29:10 +0530

    Time: 146 ms (51.47%), Space: 53.9 MB (33.20%) - LeetHub

---
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        //logic is initially we are using bricks  then if in need we will use ladders
+        priority_queue<int> pq;
+        int len=heights.size(),i=1;
+        for(;i<len;i++){
+            int diff=heights[i]-heights[i-1];
+            if(diff>0){
+                pq.push(diff);
+            bricks-=diff;
+            if(bricks<0){ //if bricks are 0 this means we have already used  bricks
+                ladders--;
+                
+                bricks+=pq.top(), pq.pop();
+                if(ladders<0 or bricks<0) return i-1;
+                
+                
+            }
+            }
+        }
+        return len-1;
+    }
+};
