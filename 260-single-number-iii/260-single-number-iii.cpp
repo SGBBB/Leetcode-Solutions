48860a1c556e6476726b56ef8cd3f54a879d3efb
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-02-16 16:22:36 +0530

    Time: 8 ms (87.23%), Space: 9.9 MB (93.25%) - LeetHub

--- 260-single-number-iii/260-single-number-iii.cpp
+++ 260-single-number-iii/260-single-number-iii.cpp
@@ -1,24 +1,35 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        int xoR = 0,n = nums.size();
-        for(int i = 0;i<n;i++) {
-            xoR ^= nums[i];
-        }
-        
-        int mask = 1;
-        while(1) {
-            if((mask & xoR) == 0) {
-                mask = mask << 1;
-            }
-            else break;
-        }
-        
-        int a = 0, b = 0;
-        for(int i = 0;i<n;i++) {
-            if(nums[i] & mask) a ^= nums[i];
-            else b ^= nums[i];
-        }
-        return {a, b}; 
+    // xor all numbers, so the duplicates are cancelled
+	int x = 0;
+	for (const int curr: nums) {
+		x ^= curr;    
+	}
+
+	// find the bit that is set in x.
+	int bit;
+	for (int i=0; i<32; ++i) {
+		if (x & (1<<i)) {
+			bit = i;
+			break;
+		}
+	}
+
+	// let the answer be first and second.
+	// let first is the number that has the bit set.
+	// second does not have the bit set, because x=first^second has the bit set.  
+	// now xor all numbers in nums with the bit set.
+	// all duplicates will be cancelled
+	// only first will remain. second will not be included, as second does not have the bit set.
+	int first = 0;
+	for (int a: nums) {
+		if (a & (1<<bit)) {
+			first ^= a;
+		}
+	}
+	// now x=first^second, therefore second = a^first
+	int second = first^x;
+	return {first, second};
     }
 };
