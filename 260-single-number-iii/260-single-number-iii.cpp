e33ebffc97f9e05619585662b1015325db09b5a1
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-02-16 17:07:54 +0530

    Time: 7 ms (90.54%), Space: 10 MB (76.63%) - LeetHub

--- 260-single-number-iii/260-single-number-iii.cpp
+++ 260-single-number-iii/260-single-number-iii.cpp
@@ -1,35 +1,24 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-    // xor all numbers, so the duplicates are cancelled
-	int x = 0;
-	for (const int curr: nums) {
-		x ^= curr;    
-	}
-
-	// find the bit that is set in x.
-	int bit;
-	for (int i=0; i<32; ++i) {
-		if (x & (1<<i)) {
-			bit = i;
-			break;
-		}
-	}
-
-	// let the answer be first and second.
-	// let first is the number that has the bit set.
-	// second does not have the bit set, because x=first^second has the bit set.  
-	// now xor all numbers in nums with the bit set.
-	// all duplicates will be cancelled
-	// only first will remain. second will not be included, as second does not have the bit set.
-	int first = 0;
-	for (int a: nums) {
-		if (a & (1<<bit)) {
-			first ^= a;
-		}
-	}
-	// now x=first^second, therefore second = a^first
-	int second = first^x;
-	return {first, second};
+        int xor1=0,xor2, n=nums.size();
+        for(int n:nums ) xor1^=n;
+        int set_bit;
+        for(int i=0;i<32;i++){
+            
+            if( xor1 & ( 1<<i )  ) 
+            {
+                set_bit=i;
+                break;
+            }
+        }
+        vector<int> ans(2);
+        for(int n:nums){
+            if( n & (1<<set_bit) )  //syntax of bitwise left shift is number<<bits to be shifted
+                ans[1]^=n;
+            else ans[0]^=n;
+        }
+        return ans;
+        
     }
 };
