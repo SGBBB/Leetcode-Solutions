702fbf205457b90be7e0f9069609fd64151de55b
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-02-17 12:56:02 +0530

    Time: 152 ms (70.60%), Space: 77.3 MB (10.30%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int n=heights.size();
-        vector<int> Left(n),Right(n);
-        Left.front()=0,Right.back()=n-1;
-        vector<int> temp;
-        for(int i=0;i<n;i++){ 
-            //calculating min_right
-            for(;!temp.empty() and heights[temp.back()]>heights[i];
+        stack<int> st;
+        int i,n=heights.size(),max_area=0;
+        for(i=0;i<=n;i++){
+            for(;  st.size() and (i==n or heights[i]<heights[st.top()])  ;){
+               int curr_height= heights[st.top()] ; 
+                st.pop();
+               int width ;
+               width=(st.empty() ) ? i  : i-st.top()-1 ; 
+               max_area=max(max_area, width* curr_height) ;
                 
-                Right[temp.back() ]=i-1,temp.pop_back() ){ }
-            temp.push_back(i);
-            
+                
+            }
+            st.push(i);
         }
-        // 0 0 0 0 0 0
-        for(;temp.size();Right[temp.back()]=n-1,temp.pop_back() ){ }
-        temp.clear();
-        //calculating left minimum
-        for(int i=n-1;i>-1;i--){
-            for(;!temp.empty() and heights[temp.back()]>heights[i];Left[temp.back()]=i+1,temp.pop_back() ){}
-            temp.push_back(i);
-        }
-        for(;temp.size(); Left[temp.back()]=0,temp.pop_back() ){ }
-        int max_ans=INT_MIN;
-        for(int i=0;i<n;i++){
-            max_ans=max(max_ans,heights[i]*(Right[i]-Left[i]+1) );
-        }
-        return max_ans;
+        
+        return max_area;        
+        // worst case complexity O(n) + O(n)
     }
 };
