07d18058e034fdf4826c626fbed7257f90bba66e
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-02-17 21:45:28 +0530

    Time: 8 ms (62.73%), Space: 10.7 MB (68.91%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    vector<vector<int>> ans;
+    void helper(vector<int>& candidates, int target,  vector<vector<int>> &ans,
+                vector<int> &temp ,int start=0,int level=0){
+        
+        if(target>-1 ) {
+            cout<<"level is "<<level<<endl;
+            if(!target) {  ans.push_back(temp);     return ;}
+            int idx,n=candidates.size();
+            for(idx=start;idx<n;idx++){
+                if(idx == start || candidates[idx] != candidates[idx-1]){
+                    temp.push_back(candidates[idx]);
+                    helper(candidates,target-                                                 candidates[idx],ans,temp,idx+1,level+1);
+                    temp.pop_back();
+                }
+                
+            }
+            
+        }
+        // if my target is <=-1 so i dont need this anymore lets backtrack from this positiioon as its dead end
+    }
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        sort(candidates.begin(),candidates.end());
+        helper(candidates , target,ans,temp);
+        return  ans;
+    }
+};
