132fb6f4d4fdfbb9501ffa111e8b88cb1e0a8bc3
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-02-20 21:11:43 +0530

    Time: 235 ms (35.71%), Space: 26.1 MB (7.14%) - LeetHub

---
+++ 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string repeatLimitedString(string s, int repeatLimit) {
+        unordered_map<char ,int > frequency ;
+        int c=0;
+        for(char ch:s) {c+=(ch=='u');frequency[ch]++;}
+        cout<<"u are "<<c<<endl;
+        cout<<"sz is "<<s.size()<<endl;
+        priority_queue<  pair<char ,int > > pq;
+        string ans="";
+        for(auto [ch,cnt]:frequency)  {cout<<ch << " "<<cnt <<endl;pq.push( {ch, cnt} );}
+        // for(;pq.size();pq.pop()) cout<<pq.top().first<<endl;
+        for( ;pq.size(); ){
+            char ch1=pq.top().first;
+            int cnt1=pq.top().second;
+            pq.pop();
+            int len=min(repeatLimit, cnt1 ),remaining=cnt1-len;
+            
+            ans+=string(min(repeatLimit, cnt1 ) , ch1 );
+            if(cnt1-repeatLimit>0){//if unchi aukat vala bnda ki freq rehti hai then 
+                char ch2;
+                int cnt2;
+                if(pq.size()){
+                    ch2=pq.top().first;
+                    cnt2=pq.top().second;
+                    pq.pop();
+                    ans+=string(1,ch2);
+                    if(cnt2-1>0) pq.push({ch2,cnt2-1});
+                }
+                else return ans;
+                pq.push({ch1,cnt1-repeatLimit});
+                
+            }
+            
+        }
+        return ans;
+    }
+};
