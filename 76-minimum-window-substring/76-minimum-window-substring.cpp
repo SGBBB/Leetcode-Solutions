b467a52597328ebe05648b20df82f033afb4f305
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-03-12 02:39:34 +0530

    Time: 26 ms (54.73%), Space: 7.6 MB (82.81%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        unordered_map<char,int> M;
+        int n1=t.size(), n2=s.size(),i=0,j=0,minLen=INT_MAX,minStart=0,cnt=n1;
+        if(n1 > n2) return "";
+        for(char  ch:t){
+            M[ch]++;            
+        }        
+        while(j<n2){
+            if(M[s[j]]>0) cnt--;
+            M[s[j]]--;
+            j++;
+            while(cnt==0){
+                if(j-i<minLen){
+                    minLen=j-i ,
+                    minStart=i;
+                }
+                M[s[i]]++;
+                if(M[s[i]]  >0 ){/*if the character is required then it will automatically get greater than 0*/
+                    cnt++;
+                }
+                i++;
+            }            
+        }        
+        return (minLen==INT_MAX)?"":s.substr(minStart,minLen);
+    }
+};
