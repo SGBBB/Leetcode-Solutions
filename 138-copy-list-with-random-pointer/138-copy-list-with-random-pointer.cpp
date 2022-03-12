85aeb29037f957346e046a9b4b59d4df03ee7bcf
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-03-12 23:34:49 +0530

    Time: 16 ms (34.25%), Space: 11.2 MB (60.48%) - LeetHub

---
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -0,0 +1,59 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(!head) return head;
+        //Now you have atleast one node 
+        Node   *temp=head;
+        
+        for(;temp;){
+            Node* nextNode=temp->next;
+            Node * newNode=new Node(temp->val);
+            // cout<<newNode->val<<endl;
+            temp->next=newNode,
+            temp=temp->next;
+            temp->next=nextNode;
+            temp=temp->next;
+            
+        }
+        //second step mei I'm assigning random pointers correspondoing to every node
+        
+        for(temp=head ;temp ;temp=temp->next->next){
+            temp->next->random= !(temp->random) ? temp->random: temp->random->next ;
+        }
+        
+        //I beleive until now everything is all good to go
+        //Finally In third step I' m unwiring Deep copy from original one(s)
+        
+        Node*newHead=head->next;
+        Node *prev=head,*curr=newHead ; 
+        
+        for(;1;){
+            prev->next=curr->next,
+            prev=curr->next;
+            if(!prev) break;//yadi prev NULL ho gya 
+            curr->next=prev->next,
+            curr=prev->next;
+        }
+        // I think I'm done writing algo 
+        for(Node* chu=head;chu;chu=chu->next)cout<<chu->val <<" "; cout<<endl;
+        for(Node* chu=newHead;chu;chu=chu->next)cout<<chu->val <<" "; cout<<endl;
+        return newHead;
+        
+    }
+};
