6bd33de39eaf9393a5b4f9709fb805a6163fbd90
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-03-14 20:05:01 +0530

    Time: 21 ms (6.05%), Space: 10.6 MB (25.77%) - LeetHub

---
+++ 71-simplify-path/71-simplify-path.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        if(path.back() !='/' ) path+='/';
+        stack<string> st;
+        // st.clear();
+        string temp="";
+        for(char ch:path){
+            if(ch!='/' ) temp+=ch;
+            if(ch=='/'){
+                if( temp=="" or temp=="." ) {
+                    //ignore;
+                }
+                else if( temp==".."){
+                    if(st.size() )st.pop();
+                }
+                else st.push(temp);
+                temp="";//reset temp
+            } 
+        }
+        string ans="";
+        for(;st.size() ;  st.pop() ){
+            cout<<" stack guys are  "<<st.top() <<endl;
+            ans='/' +st.top()  + ans;
+        }
+        if(ans.empty() ) return "/";
+        return ans;
+    }
+};
