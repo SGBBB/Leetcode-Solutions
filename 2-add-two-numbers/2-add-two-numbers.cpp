bb1e4462daf25740510a85675ae5fbc45c452e9f
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-03-16 11:44:10 +0530

    Time: 47 ms (59.20%), Space: 71.6 MB (11.75%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -12,12 +12,11 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2,int carry=0) {
         // cout<<l1->val<<" "<<l2->val<<endl;
-        if(l1 or l2 or carry>0){
-            int sum=carry;
-            sum+=l1 ? l1->val : 0 ;
-            sum+=l2?  l2->val:0 ;  
-            ListNode* temp=new ListNode(sum%10);
-            temp->next= addTwoNumbers(l1?l1->next:NULL,l2?l2->next:NULL ,sum/10) ;
+        if(l1 or l2 or carry>0){ 
+            carry+=l1 ? l1->val : 0 ;
+            carry+=l2?  l2->val:0 ;  
+            ListNode* temp=new ListNode(carry%10);
+            temp->next= addTwoNumbers(l1?l1->next:NULL,l2?l2->next:NULL ,carry/10) ;
             return temp;
         }
         return NULL;
