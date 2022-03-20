75ae350e922d6103bc806254ba02cb60320f7fa5
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-03-20 15:47:45 +0530

    Time: 57 ms (100.00%), Space: 16.3 MB (75.00%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
@@ -1,64 +1,17 @@
 class Solution {
 public:
-    int countCollisions(string directions) {
-        //bc konsa ds use kru?? 
-        /*an important thing R can be chopped by L but vica versa is nt gonna happen 
+    int countCollisions(string directions,int i=0,int j=0) {
+        int n=directions.size();
+        j=n-1;
+        while(i<n and directions[i]=='L' )   i++; 
         
-        
-        should i use stack to cater this problme 
-        
-        
-        
-        */
-        
-        int  cnt=0;
-        stack<char> st;
-        for(char &ch:directions){ 
-            // if(st.empty() ) {
-            //     st.push(ch);
-            //     continue;
-            // }
-            //conditions
-            // if(!st.empty()){
-            //     bool a=(st.top()=='R' && ch=='L'),
-            //     b= 
-            // }
-            // while(!st.empty() && 
-            //       ( (st.top()=='R' && ch=='L') || ( st.top()=='S' && ch=='L' ) ||( st.top()=='R' && ch=='S' ) )
-            //      ){
-            //     if(st.top()=='R' && ch=='L') cnt+=2;
-            //     else if( ( st.top()=='S' && ch=='L' ) ||( st.top()=='R' && ch=='S' ))                           cnt+=1;
-            //     st.pop();
-            // } 
-            // st.push(ch);
-            
-//             if(!st.empty() && ch=='L'){
-//                 if( st.top()=='R' ) cnt+=2,st.pop(),st.push('S');
-//                 else if(st.top()=='S' ) cnt++,st.pop(),st.push('S');
-//                 else st.push(ch);
-//             }
-//             // else if(!st.empty() && ch=='R'){
-//             //     // if(st.)
-//             // }
-            
-//             else if(!st.empty() && ch=='S') { 
-//                 if(st.top()=='R') cnt++,st.pop(),st.push('S');
-//                 else st.push(ch);
-//             }
-//             else st.push(ch);//if ch 'R' ho ya  stack empty ho toh sidha push kro
-            
-                if(st.size( ) and ch=='L' &&  st.top()=='R') 
-                     cnt+=2, st.pop() ,ch='S' ;
-                else if(st.size( ) and  ch=='L' && st.top()=='S')
-                     cnt+=1, st.pop() ,ch='S' ; 
-            
-            
-            
-            // cout<< cnt<<" st size is "<<st.top() <<st.size()<<" "<<ch<<endl;
-            while(!st.empty() and ch=='S' && st.top()=='R')
-            {cnt++,st.pop();}
-            st.push(ch); 
-            
+        while(j>-1 and directions[j]=='R')  
+            j--;
+        int cnt=0;
+        cout<<i<<j<<endl;
+        while(i<j+1){
+            cnt+=( directions[i]!='S' );
+            i++;
         }
         return cnt;
     }
