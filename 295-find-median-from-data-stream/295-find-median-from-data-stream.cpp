b3649d4f788e93bed0cf36365de14a3a1defa21d
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-03-21 23:11:15 +0530

    Time: 590 ms (21.69%), Space: 117.1 MB (22.36%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.cpp
@@ -5,8 +5,10 @@ public:
     
     MedianFinder() { }
     void addNum(int num) {
+        //initially I'm. ensuring that  let's insert value in maxHeap or if valuen is less than maxHeap ka top value then insert it to maxHeap else insert it to minHeap.  
         if( maxHeap.empty() || num <maxHeap.top() ) maxHeap.push(num);
         else minHeap.push(num);
+        //if the maxHeap.size() went too bigger than minHeap then do transfer the element frommaxHeap to minHeap.
         if( maxHeap.size() >minHeap.size()+1     ) 
             //do something
             minHeap.push(maxHeap.top()) , maxHeap.pop() ;
@@ -15,8 +17,10 @@ public:
             maxHeap.push(minHeap.top() ),minHeap.pop() ; 
     }
     double findMedian() {
+        //if size of both heaps is same then that means both heaps has even size so overall size is even too so median will be average of maxHeap.top() and minHeap.top()
         if(minHeap.size()==maxHeap.size()  ) 
             return   (double )(maxHeap.top()+minHeap.top() )/2; 
+        //else if there exists difference between the size of both the heaps then ans will be top guy of the larger size heap
         else if(maxHeap.size() >minHeap.size()) return (double)maxHeap.top() ;
         return (double)minHeap.top() ;
     }
