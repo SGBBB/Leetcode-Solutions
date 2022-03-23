7097fc6d9b0d13b34d426f4042c2724bbb35e7b4
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-03-23 16:24:42 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (28.57%) - LeetHub

--- 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.cpp
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.cpp
@@ -1,15 +1,22 @@
 class Solution {
-    
-    
 public:
     int minMoves(int target, int maxDoubles) {
-        int min_moves=0;
-        for(;target>1 and maxDoubles;){
-            if(target & 1) target--;
-            else maxDoubles--,target/=2 ;
-            min_moves++;
+        /*
+        Let me use recursion to crack this problem
+        
+        */
+        if(target>1){
             
+            return (
+                
+(!maxDoubles) ?  target-1 :
+              
+( target&1 )  ?
+    minMoves(target-1,maxDoubles)+1: 
+                minMoves(target/2,maxDoubles-1)+1
+                    
+                    );
         }
-        return min_moves + target-1;
+        return 0;//ok fine now you reached to 1 just return 0 which means no more moves are needed
     }
 };
