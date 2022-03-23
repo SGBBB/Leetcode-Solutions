4fe053ceaa4b2682387187eb59125b5e1b9c48d4
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-03-23 15:49:37 +0530

    Time: 3 ms (32.55%), Space: 5.9 MB (73.18%) - LeetHub

--- 991-broken-calculator/991-broken-calculator.cpp
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -14,11 +14,19 @@ public:
         test for the following test cases:
         * 3 19
         
+        while going back to the startValue
+        i'm eleiminating the huge values  by just half 
+        So time complexity turns out to be log(target).
         
         */
         if(startValue>=target) return startValue-target;
-        int response= (target & 1) ?  brokenCalc(startValue,target+1) :
-                brokenCalc(startValue,target/2)   ; 
-        return response+1;
+        /* 
+        if the number is odd then add 1 to it to make it even 
+        
+        */
+        return 1+((target & 1) ?  brokenCalc(startValue,target+1) :
+                  
+                brokenCalc(startValue,target/2) )  ; 
+        
     }
 };
