e5cf3113aec935eb59d8c17a52e98c4127c2bf72
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-03-23 11:55:38 +0530

    Added solution - LeetHub

--- Minimize the Heights II - GFG/minimize-the-heights-ii.cpp
+++ Minimize the Heights II - GFG/minimize-the-heights-ii.cpp
@@ -10,8 +10,7 @@ using namespace std;
 class Solution {
   public:
     int getMinDiff(int arr[], int n, int k) {
-        sort(arr,arr+n); 
-        
+        sort(arr,arr+n);
         int ans = arr[n-1] - arr[0]; //initially after sorting tallest tower = arr[n-1] and smallest tower = arr[0]
         
         int smallest = arr[0] + k; //we have to increase smallest tower by k
@@ -19,15 +18,19 @@ class Solution {
         
         int mini,maxi;
         
-        for(int i = 0; i < n-1; i++) {
+        for(int i = 0; i < n-1; i++) { 
             mini = min(smallest, arr[i+1] - k); //minimum right tower 
             maxi = max(tallest, arr[i] + k); //maximum left tower ,if you found greater maxi and difference went down so dont consider it
-            
+            // cout<<" maxi and mini are "<<mini<<" "<<maxi<<endl;
             if(mini < 0) continue;
             // cout<<   mini<<" "<<maxi << endl;
+            //If diff. btw maxi and mini is too huge then don't consider it !
             ans = min(ans, maxi - mini);
         }
+        /*
+        Try this test case
         
+        */
         return ans;
     }
 };
