276dac6fc6fe69ac4a35f27450ff25e51392d981
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-03-24 17:03:46 +0530

    Time: 8 ms (63.09%), Space: 13 MB (60.97%) - LeetHub

---
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    map<TreeNode* ,TreeNode *> M;
+    void markParents(TreeNode* root){
+        if(!root) return ;
+        if(root->left) {
+            M[root->left]=root;
+            markParents(root->left);
+        }
+        if(root->right){
+           M[root->right]=root;
+           markParents(root->right); 
+        }        
+    }
+public:
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        vector<int> ans;
+        if(!root )return ans;
+        markParents(root);
+        map<TreeNode* ,int> visited;
+        queue<TreeNode*> q;
+        q.push(target);        
+        visited[target]=true;
+        while(q.size()>0){
+            /*breaking once k==0*/    
+            if(!k) break;
+            int n=q.size();
+            for(int i=0;i<n;i++){
+                auto p= q.front();
+                visited[p]=true;
+                q.pop();
+                if(M.count(p)   and !visited[M[p]]){
+                    q.push(M[p]),
+                    visited[M[p]]=true;
+                    
+                }
+                if(p->left and !visited[p->left]) {
+                    q.push(p->left),
+                    visited[p->left]=1;
+                }
+                if(p->right and !visited[p->right]) {
+                    q.push(p->right),
+                    visited[p->right]=1;;
+                }
+            }
+            k--;
+        }
+        while(q.size()>0){
+            ans.push_back(q.front()->val);
+            q.pop();
+        }
+        return ans;
+    }
+};
