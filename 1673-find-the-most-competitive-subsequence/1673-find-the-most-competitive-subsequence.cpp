5f98de66e6cda01b8a46aee40918680aeed420eb
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-03-27 07:49:08 +0530

    Time: 257 ms (67.21%), Space: 109.8 MB (70.46%) - LeetHub

--- 1673-find-the-most-competitive-subsequence/1673-find-the-most-competitive-subsequence.cpp
+++ 1673-find-the-most-competitive-subsequence/1673-find-the-most-competitive-subsequence.cpp
@@ -3,7 +3,11 @@ public:
     vector<int> mostCompetitive(vector<int>& A, int k) {
         vector<int> stack;
         for (int i = 0; i < A.size(); ++i) {
-            while (!stack.empty() && stack.back() > A[i] && stack.size()  + A.size() - i > k)
+            /*
+            "stack.size()  + A.size() - i > k" this line is checking whether we have enough eelements to sustain which i sgreater than k.
+            
+            */
+            while (!stack.empty() && stack.back() > A[i] && stack.size()  + A.size() - i > k) 
                 stack.pop_back();
             if (stack.size() < k)
                 stack.push_back(A[i]);
