bbc885ef7f55765bfb9ea2ba36c955617c416f40
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-03-27 07:30:15 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (97.37%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-        vector<int> freq(26);
-        for(char ch:s){
-            freq[ch-'a']++;
-        }
-        vector<bool> vis(26,false);
-        stack<int> st;
-        for(int i=0;i<s.size();i++){            
-            char ch=s[i];            
-            if( vis[ch-'a'] ) {  freq[ch-'a']--;  continue;}
-            vis[ch-'a']=true;
-            while(st.size() and s[st.top()]>ch and freq[s[st.top()]-'a']>1 ){
-                freq[s[st.top()]-'a']--;
-                vis[s[st.top()]-'a']=false;
-                st.pop(); 
-            }
-            st.push(i);
-        }        
         string res;
-        while(st.size() ){
-            res=  s[st.top() ]+ res ;
-            st.pop();
-        }
-        
-        return res;
+        int cnt[26] = {}, used[26] = {};
+  for (auto ch : s) ++cnt[ch - 'a'];
+  for (auto ch : s) {
+    --cnt[ch - 'a'];
+    if (used[ch - 'a'] > 0) continue;
+    while (!res.empty() && res.back() > ch && cnt[res.back() - 'a'] > 0) {
+      used[res.back() - 'a'] = 0;
+      res.pop_back();
+    }
+    res.push_back(ch);
+      used[ch-'a']=1;
+  }
+  return res;
     }
 };
