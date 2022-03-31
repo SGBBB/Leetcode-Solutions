d52b200fb7540cc2da374b6bb606aa5d2bcd44d2
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-04-01 00:04:04 +0530

    Time: 25 ms (57.30%), Space: 13.3 MB (60.03%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
@@ -5,6 +5,12 @@ public:
         for(int i:nums) M[i]++;
         int cnt=0;
         for(auto [key,value]:M){
+            /*
+            bc 2 cases bnre h 
+            1.  ( !k and M[key]>1 ) this means if k is 0 then M[key ] i mean frequency of key must be greater than 1 becoz key-key==0
+            2. if k > 0  ,then find whether key -k exists or not?????
+            
+            */
             if( ( !k and M[key]>1 ) or (k>0 and M.count(key-k) )  ) cnt++;
             
         }
