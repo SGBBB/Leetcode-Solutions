a4ff9a3c01d380c7c2539ce4dc967c37a08600af
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-04-02 17:23:29 +0530

    Time: 74 ms (40.41%), Space: 19.6 MB (87.28%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -1,22 +1,21 @@
 class Solution {
     public:
-bool flag = 0;
     bool validPalindrome(string s) {
         return helper(s, 0, s.size() - 1);
     }
 
-    bool helper(string &s, int l, int r) {
+    bool helper(string &s, int l, int r,bool can_del = 1) {
         if(l >= r)
             return true;
 
         // same
         if(s[l] == s[r])
-            return helper(s, l+1, r-1);
+            return helper(s, l+1, r-1,can_del);
         // Not same for 2nd time
-        if(flag)
+        if(!can_del)
             return false;
         // not same for first time
-        flag = 1;
-        return helper(s, l+1, r) || helper(s, l, r-1);
+       
+        return helper(s, l+1, r,0) || helper(s, l, r-1,0);
     }
 };
