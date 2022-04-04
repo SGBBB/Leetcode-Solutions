bd3bc978afb9c6ddc962e2a3b21c4e74d1842c4b
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-04-05 01:38:34 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (73.79%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -2,7 +2,6 @@ class Solution {
     int f(vector<int>& v, int m,int M){
         int cnt=1,sum=0;
         for(int x:v){
-            // if(x>M) return 0;
             if(sum+x>M) {sum=x;cnt++;}
             else sum+=x;
             
@@ -11,9 +10,16 @@ class Solution {
     } 
 public:
     int splitArray(vector<int>& nums, int m) {
+        /*
+        Generally in these variety of problems ans lies in search space if we have thought about search space most of the work is done by our side.
+        intuition --->we are building is we are building subarrays with certain values decided while using binary search technique
+        every time we are checkcing whether m length subarrays are possible if we consider subarrays having sum as mid value we are passing.
+        
+        */
         int l=*max_element(nums.begin(),nums.end())  ,  h=accumulate(nums.begin(),nums.end(),0),ans=0;
         while(l<=h){
             int mid=l+(h-l)/2;
+            cout<<"mid is "<<l<<mid<< h<<endl;
             if(f(nums,m,mid)){ // subarray count is less than equals to given m
                 ans=mid;
                 h=mid-1;
