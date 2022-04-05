9bc6ed094241d0aecf3c1abaa6a6b33b548c638f
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-04-06 02:05:40 +0530

    Time: 2 ms (39.90%), Space: 5.7 MB (96.08%) - LeetHub

--- 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.cpp
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.cpp
@@ -14,7 +14,7 @@ public:
 ( target&1 )  ?
     minMoves(target-1,maxDoubles)+1: 
                 minMoves(target/2,maxDoubles-1)+1
-                    
+          //here im doing +1   so that contribution current step is also considered  while returning back to parent function call     
                     );
         }
         return 0;//ok fine now you reached to 1 just return 0 which means no more moves are needed
