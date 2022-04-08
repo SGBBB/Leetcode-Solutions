17fa724ab32fdc6c085017f3e832bc173fb0504d
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-04-09 00:33:33 +0530

    Time: 883 ms (27.83%), Space: 160.3 MB (18.68%) - LeetHub

--- 2135-count-words-obtained-after-adding-a-letter/2135-count-words-obtained-after-adding-a-letter.cpp
+++ 2135-count-words-obtained-after-adding-a-letter/2135-count-words-obtained-after-adding-a-letter.cpp
@@ -1,9 +1,6 @@
-// struct Node{
-//     Node* arr[26];
-//     int flag=0;
-    
-// };
-// Node* root=new Node();
+// ackt 1
+// act 0
+// acit 1
 struct Trie {
     Trie* ch[26] = {};
     bool end = false;
@@ -18,11 +15,26 @@ struct Trie {
             end = true;
     }
     bool find(string &s, int p = 0, bool wild = false) {
-        if (p == s.size())
+        // cout<< s[p]<<"  "<< p<< " "<< wild<< " end is "<<end << endl;
+        if (p == s.size()){ 
+            cout<<"reached end "<< " "<< s <<" "<<(wild & end)<<endl;
+            //When p points to s.size() then my wild and end must be true 
             return wild && end;
+        }
+      
         int idx = s[p] - 'a'; 
-return (ch[idx] != nullptr ? ch[idx]->find(s, p + 1, wild) : false) || (wild ? false : find(s, p + 1, true));
+        
+        // cout<< s[p]<<"  "<< p<< " "<< wild<< "  end is "<< end<<endl;
+        bool res=(ch[idx] != nullptr ? ch[idx]->find(s, p + 1, wild) : false) ||
+            (wild ? false : find(s, p + 1, true)) ;
+        /*
+        
+        find(s, p + 1, true) is calling to current level by marking wild true
+        */
+        
+        return res;
     }
+    
 };
 class Solution {
     public:
@@ -35,51 +47,11 @@ class Solution {
     int res = 0;
     for (auto &w : targetWords) {
         sort(begin(w), end(w));
-        cout<< w<<" "<<t.find(w) <<endl;
+        // cout<< w<<" "<<t.find(w) <<endl;
+        // break;
         res += t.find(w);
     }
     return res;    
 }
-//     int alphabets="abcdefghijklmnopqrstuvwxyz";
-//     void insert_node(string word){
-//         Node* node=root;
-//         for(char ch:word){
-//             if(!node->arr[ch-'a'] )
-//                 node->arr[ch-'a']=new Node();
-//             node=node->arr[ch-'a'];
-//         }
-//         node->flag=1;
-//     }
-//     bool find_nodes(string word){
-//         // vector<bool> visited(26,false);
-//         queue<string>  q(word.begin() ,word.end() );
-//         int rotations=0;
-//         Node* node=root;
-//         while(q.size() and rotations<q.size() ){
-//             char ch=q.front(); q.pop();
-//             for(char letter:alphabets ){
-//             if(  letter==ch and   node->arr[ch-'a'] )
-//                 node=node->arr[ch-'a']; //updation
-//             else 
-//         }
-            
-//         }
-        
-        
-//         return node->flag;
-//     }
-    
-// public:
-//     int wordCount(vector<string>& startWords, vector<string>& targetWords) {
-        
-//         root=new Node();
-//         for(auto word:startWords){
-//             insert(word);  
-//         }
-//         int cnt=0;
-//         for(auto word:targetWords){
-//             cnt+=find(word);
-//         }
-//         return cnt;
-//     }
+
 };
