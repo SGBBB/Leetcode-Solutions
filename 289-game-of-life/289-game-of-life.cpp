3c3c8129cc9e1478c14edb88d8e9cdb4a182f2a9
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-04-13 01:51:30 +0530

    Time: 0 ms (100.00%), Space: 7 MB (58.40%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -10,6 +10,10 @@ public:
           0->  1  is called 2
           1->  0  is called 3
          
+         <2 or >3 live neighbourer die
+         2 or 3 live neighbourer live
+         dead cell with 3 live neighbourer become live
+         
         */
         vector<pair<int ,int> > dir=
         {{-1,0 } , { 1,0} , {0,-1} , {0,1} ,
@@ -30,6 +34,7 @@ public:
                 }
                 cout<<cnt<<endl;
                 if(board[i][j]==1) {
+                    //Any live guy with cnt>3 or cnt<2 will die so assign board[i][j]=3
                         board[i][j]=(cnt>3 or cnt<2) ?3: 1;
                     }
                     else{
@@ -40,11 +45,11 @@ public:
         }
         for(auto &i:board){
             for(auto &j:i){
-                // cout<<j<<" ";
-                if( !(j==0 or j==1 ) ){
+                // if val at cell isnt 1 or 0 
+                if( !(j==0 or j==1 ) ){ 
                     j=(j==2) ?1: 0; 
                 }
-            }cout<<endl;
+            }
         }
         
         
