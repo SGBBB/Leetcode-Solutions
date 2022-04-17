9b528ca975aa5a5028d68a150812daaf831e7162
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-04-17 23:43:37 +0530

    Time: 99 ms (7.11%), Space: 7.1 MB (13.26%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -7,12 +7,12 @@ public:
             cnt[s[j]-'A']++;
             if(cnt[s[j]-'A'] >maxRepeating ) maxRepeating=cnt[s[j]-'A'];//here Im updating my maxRepeating guy
             /*length of current substring -count of repeating characters <=k ( no of times operation is performed) */
-            while( j-i+1-maxRepeating > k ){
+            if( j-i+1-maxRepeating > k ){
                 cnt[s[i]-'A']--;
                 i++;
             }
-            
-            maxLen=max(maxLen , j-i+1);
+            else
+                maxLen=max(maxLen , j-i+1);
             j++;
             cout<<maxRepeating<<endl;
         }
