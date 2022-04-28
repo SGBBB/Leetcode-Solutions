2772116c58c9ed74c213e8eb412e89dd57649c08
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-04-28 20:40:06 +0530

    Time: 7 ms (42.10%), Space: 11.2 MB (55.30%) - LeetHub

--- 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.cpp
+++ 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.cpp
@@ -1,26 +1,40 @@
 class Solution {
+//     vector<int> computeExpression(string &expression,int i,int j){
+//         for(int k=i+1;k<j;k+=2){
+            
+//             if( expression[k]=='-' ){
+                
+//             }
+//             computeExpression()
+//         }
+//     }
 public:
     vector<int> diffWaysToCompute(string expression) {
-        vector<int> response;
+        // return computeExpression(expression,0,expression.size()-1);
         int n=expression.size();
+        vector<int> ans;
         for(int i=0;i<n;i++){
-            char ch= expression[i];
-            if(!isdigit(ch)){
-                //I need response from left and right subarrays
-                vector<int> left_response=diffWaysToCompute(expression.substr(0, i-0) );
-                vector<int> right_response=diffWaysToCompute(expression.substr(i+1));
-                for(int i:left_response){
-                    for(int j:right_response){
-                        int result=(ch=='+')?  i+j:ch=='*' ? i*j: i-j ;
-                        response.push_back(result);
+            char ch=expression[i];
+            if(!isdigit(ch)){ //At this moment my ch is surely an operator
+                vector<int> Left=diffWaysToCompute(expression.substr(0,i));
+                vector<int> Right=diffWaysToCompute(expression.substr(i+1));
+                
+                for(auto i:Left){
+                    for(auto j:Right){
+                        if (ch=='+')
+                            ans.push_back(i+j);
+                        else if (ch=='-')
+                            ans.push_back(i-j);
+                        else 
+                            ans.push_back(i*j);
                     }
+                        
                 }
-                
-                
                 
             }
         }
-        
-        return ( !response.empty() ) ? response  : vector<int>{stoi(expression) };
+        if ( !ans.empty() )  return  ans;
+        ans.push_back(stoi(expression));
+        return ans;
     }
 };
