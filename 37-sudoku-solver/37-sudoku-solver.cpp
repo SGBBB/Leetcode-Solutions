7b913e7153d139a04c2b3c383501541c42ee1a51
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-05-05 23:29:11 +0530

    Time: 39 ms (47.29%), Space: 6.4 MB (55.10%) - LeetHub

---
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool isValid(int row,int col,vector<vector<char>>& board,char c)
+    {
+        for(int i=0;i<9;i++)
+        {
+            if(board[row][i]==c)
+                return false;
+            if(board[i][col]==c)
+                return false;
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c)
+                return false;
+        }
+        return true;
+    }
+    bool solve(vector<vector<char>>& board )
+    {
+        
+         for(int i=0;i<9;i++)
+        {
+            for(int j=0;j<9;j++)
+            {
+                if(board[i][j]=='.')
+                {
+                    for(char c='1';c<='9';c++)
+                    {
+                        if(isValid(i,j,board,c))
+                        {   //cout<< c<<endl;
+                            board[i][j]=c;
+                            if(solve(board ))
+                             {return true;}
+                            board[i][j]='.';  
+                        }
+                    }
+                    //im unable to fill  cell so im returning false;
+                    return false;
+                }
+            }
+        }
+        //bhenchod i have filled all cells so returning true
+        return true;
+    }
+    void solveSudoku(vector<vector<char>>& board) {
+     solve(board);
+    }
+};
