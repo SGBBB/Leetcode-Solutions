c6721e85a9e31a3cafcc536b7fbbfc5295871640
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-05-06 21:38:48 +0530

    Time: 25 ms (68.41%), Space: 11.1 MB (27.21%) - LeetHub

---
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    
+    string removeDuplicates(string s, int k) {
+        vector<pair<char, int>> st;
+        string res;
+        for (auto ch : s) {
+            //some business logic
+            // "abbbaa"
+            //  3
+            if(!st.empty()  && st.back().first==ch){
+                if( ++st.back().second==k )
+                    st.pop_back();
+            }
+            else 
+                st.push_back({ch,1});
+            
+              
+        }
+        for(auto it:st){
+            //everytime creating a new string and appending to existing res;
+            res+=string(it.second,it.first);
+        }
+        return res;
+    }
+};
+/*
+How will I move ahead int his problelm??????
+
+
+        "deeedbbcccbdaa"
+        initially remove k duplicates ,you will receive a new string pass it to the recursive function 
+        
+        
+        
+    This whole process is gonna take O(n^2) complexity.
+    "deeedbbcccbdaa"->"ddbbbdaa"->"dddaa"->"dddaa"=>"aa" this is my final ans.
+    One clue is clear that s only contains lower case English letters.
+    How can you leverage this fact?????
+    Can you use stack data structure to solve this problem upto some extent.
+    
+    "deeedbbcccbdaa"
+    
+    
+*/
