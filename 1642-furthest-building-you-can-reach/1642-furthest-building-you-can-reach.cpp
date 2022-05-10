6047d8e7c458e9d8e5e771767d22d32f5da0a1d0
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-05-10 11:59:22 +0530

    Time: 182 ms (20.44%), Space: 69.5 MB (5.89%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
         //logic is initially we are using bricks  then if in need we will use ladders
-        priority_queue<int> pq;
+        multiset<int> pq;
         int len=heights.size(),i=1;
         for(;i<len;i++){
             int diff=heights[i]-heights[i-1];
             if(diff>0){
-                pq.push(diff);
+                pq.insert(diff);
             bricks-=diff;
             if(bricks<0){ //if bricks are 0 this means we have already used  bricks
                 ladders--; //if im short of bricks then use ladders
                 
-                bricks+=pq.top(), pq.pop();
+                bricks+=*--pq.end(), pq.erase( --pq.end());
+                // eventually i have lost both bricks as well as ladders thats why i can only reach until here
                 if(ladders<0 or bricks<0) return i-1;
                 
                 
