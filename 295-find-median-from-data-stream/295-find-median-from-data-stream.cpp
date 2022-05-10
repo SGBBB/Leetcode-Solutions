3d5f9a582790324e0af96205a41764dc3c06306f
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-05-10 12:30:29 +0530

    Time: 371 ms (72.56%), Space: 123 MB (11.06%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.cpp
@@ -1,29 +1,33 @@
 class MedianFinder {
-public:
+    public:
+        MedianFinder() { }
+
     priority_queue<int> maxHeap;
-    priority_queue<int, vector<int>, greater<int>> minHeap;
+    multiset<int> minHeap;
     
-    MedianFinder() { }
+    
     void addNum(int num) {
-        //initially I'm. ensuring that  let's insert value in maxHeap or if valuen is less than maxHeap ka top value then insert it to maxHeap else insert it to minHeap.  
+        //Connecting such that maxHeap is placed before minHeap
+        //initially I'm. ensuring  let's insert value in maxHeap or if value is less than maxHeap ka top value then insert it to maxHeap else insert it to minHeap.  
         if( maxHeap.empty() || num <maxHeap.top() ) maxHeap.push(num);
-        else minHeap.push(num);
-        //if the maxHeap.size() went too bigger than minHeap then do transfer the element frommaxHeap to minHeap.
+        else minHeap.insert(num);
+        //if the maxHeap.size() went too bigger than minHeap then do transfer the element from maxHeap to minHeap.
         if( maxHeap.size() >minHeap.size()+1     ) 
-            //do something
-            minHeap.push(maxHeap.top()) , maxHeap.pop() ;
+            
+            minHeap.insert(maxHeap.top()) , maxHeap.pop() ;
             
         else if(minHeap.size() > maxHeap.size() +1) 
-            maxHeap.push(minHeap.top() ),minHeap.pop() ; 
+            maxHeap.push( *minHeap.begin() ), minHeap.erase( minHeap.begin() ) ; 
     }
     double findMedian() {
-        //if size of both heaps is same then that means both heaps has even size so overall size is even too so median will be average of maxHeap.top() and minHeap.top()
+        //if size of both heaps is same then that means overall size is even . so median will be average of maxHeap.top() and minHeap.top()
         if(minHeap.size()==maxHeap.size()  ) 
-            return   (double )(maxHeap.top()+minHeap.top() )/2; 
+            return   (double )(maxHeap.top()+ *minHeap.begin() )/2; 
         //else if there exists difference between the size of both the heaps then ans will be top guy of the larger size heap
-        else if(maxHeap.size() >minHeap.size()) return (double)maxHeap.top() ;
-        return (double)minHeap.top() ;
+        else if( maxHeap.size() > minHeap.size() ) return (double) maxHeap.top() ;
+        return (double) *minHeap.begin() ;
     }
+    
 };
 
 /**
