9e744ff796bfc7b989aa06440bc339bb7305b6d3
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-05-24 01:19:00 +0530

    Time: 4 ms (53.10%), Space: 7.1 MB (73.53%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -14,19 +14,22 @@ public:
         Generally in these variety of problems ans lies in search space if we have thought about search space most of the work is done by our side.
         intuition --->we are building is we are building subarrays with certain values decided while using binary search technique
         every time we are checkcing whether m length subarrays are possible if we consider subarrays having sum as mid value we are passing.
-        
+        blackbox is returning whether there exists minimal
+        largest subarray 
+        l is max element becoz at worst case im assuming thee will be just n subarays
+        and h=sum of every lelemtn becoz at good case im having just 1 subarray
         */
         int l=*max_element(nums.begin(),nums.end())  ,  h=accumulate(nums.begin(),nums.end(),0),ans=0;
         while(l<=h){
             int mid=l+(h-l)/2;
-            cout<<"mid is "<<l<<mid<< h<<endl;
-            if(f(nums,m,mid)){ // subarray count is less than equals to given m
+            cout<<"mid is "<<l<<" "<<mid<<" "<< h<<endl;
+            if( f(nums,m,mid) ){ // subarray count is less than equals to given m ,so i'll shrink to left so that to accomodate less sum more subarrays will. be required
                 ans=mid;
                 h=mid-1;
             }
             else l=mid+1;
         }
-        return l;
+        return ans;
         
     }
 };
