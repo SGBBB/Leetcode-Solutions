09a159556187fe4caaf7b88c217ee32a03b15e22
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-05-30 18:05:51 +0530

    Time: 218 ms (6.48%), Space: 41.1 MB (5.61%) - LeetHub

---
+++ 638-shopping-offers/638-shopping-offers.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    /*
+    [2,3,4]
+    
+    [1,1,0,4]
+    [2,2,1,9]
+    
+    [1,2,1]
+     
+     price = [2,5], special = [[3,0,5],[1,2,10]], needs = [3,2]
+     
+     
+     (3,2)
+     
+    I think special array we hav eto save in map only 
+    
+    */
+    
+int shoppingOffers(vector<int>& price, vector<vector<int>>& special,vector<int> needs) {
+        
+        int n=price.size();
+        int min_cost=0;
+        for(int i=0;i<n;i++){
+            min_cost+=(price[i]*needs[i]);
+        }
+        
+        // Iterating to each special offers 
+        for(auto it:special){
+            vector<int> needs_subtracted(n);
+            int flag=1;
+            for(int i=0;i<n;i++){
+                if(it[i]>needs[i]){
+                    flag=0; break;  
+                }
+                else 
+                    needs_subtracted[i]=needs[i]-it[i];
+            }
+            // current discount isnt appropriate
+            if (!flag)
+                continue;
+            
+            // now cur shopping offer is appropriate
+            
+            int cur_cost=it[n] +shoppingOffers(price,special,needs_subtracted);
+            min_cost=min( { min_cost,cur_cost } );
+            
+        }
+        return min_cost;
+        
+    }
+};
