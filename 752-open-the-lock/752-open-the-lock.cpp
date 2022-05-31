a93dad1c4ed23e033de4120d35eb6a62720ba099
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-05-31 19:34:57 +0530

    Time: 169 ms (85.85%), Space: 21 MB (93.34%) - LeetHub

--- 752-open-the-lock/752-open-the-lock.cpp
+++ 752-open-the-lock/752-open-the-lock.cpp
@@ -1,14 +1,71 @@
 class Solution {
 public:
+    int openLock(vector<string>& deadends, string target) {
+        set<string> deadStates(deadends.begin(),deadends.end()),
+        startStates,
+        endStates;
+        
+        startStates.insert("0000");
+        endStates.insert(target);
+        if(deadStates.count( string(4,'0') ) )
+            return -1;
+        int level=0;
+        while( !startStates.empty() && !endStates.empty()){
+            
+            set<string> tempStates;
+            for(auto &str:startStates){
+                
+                if(deadStates.count(str))
+                    continue;
+                deadStates.insert(str);
+                if( endStates.count(str) )
+                    return level;
+                
+            
+            //If you found some state in the. mid-way then you must return the cnt
+            
+            string curStr=str;
+            for(auto &ch:curStr){
+                int num=ch-'0',
+                prev_num=(num+10-1)%10,
+                next_num=(num+1)%10;
+                ch=prev_num+'0';
+                if( !deadStates.count(curStr) ){
+                    
+                    tempStates.insert(curStr) ;
+                }
+                
+                ch=next_num+'0';
+                
+                if( !deadStates.count(curStr) ){
+                    
+                    tempStates.insert(curStr) ;
+                }
+                ch=num+'0';//changing ch to its original position
+            } //end of for loop
+            
+                
+            }
+            
+            
+            // update sets
+            level++;
+            startStates=endStates,
+            endStates=tempStates;
+        }
+        
+        return -1;
+    }
     /*
-    At max I will be ended -up explroring 1000 states.
+    At max I will be ended-up explroring 1000 states.
     0000->
     
     
     On what basis am I making next move????
     
     */
-    int openLock(vector<string>& deadends, string target) {
+    
+    int openLock_method2(vector<string>& deadends, string target) {
         set<string> S(deadends.begin(),deadends.end());
         queue<vector<string>>q; //{states,cnt}
         q.push({"0000","0"});
