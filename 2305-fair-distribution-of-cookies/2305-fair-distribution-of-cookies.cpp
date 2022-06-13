2523554c6555a51db26be6cff8a27c6185fc2f17
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-06-13 12:29:45 +0530

    Time: 8 ms (100.00%), Space: 6.9 MB (100.00%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -1,33 +1,36 @@
 class Solution {
-    // int minUnfairness=INT_MAX;
-//     void dfs(vector<int>& cookies, int k,vector<int> &cookiesDistribution,int idx ){
-//         //At every idx Ihave got 8 choices at max 
-//         // So Overall Complexity is gonna be k^n 
-//         // cout<<"idx   " <<idx  <<endl;
-//         if(idx==-1){
+    int minUnfairness=INT_MAX;
+    void dfs(vector<int>& cookies, int k,vector<int> &cookiesDistribution,int idx ){
+        //At every idx Ihave got 8 choices at max 
+        // So Overall Complexity is gonna be k^n 
+        // cout<<"idx   " <<idx  <<endl;
+        if( idx==cookies.size() ){
     
-//             //Perform tasks
-//             int largeAns=INT_MIN;
-//             for(auto curChildCookieCnt:cookiesDistribution){
-//                 largeAns=max( {largeAns,curChildCookieCnt} );
-//             }
-//             // cout<<largeAns<<endl;
-//             minUnfairness=min({minUnfairness,largeAns});
-//             return;
-//         }
-//         for(int i=0;i<k;i++){
-//             cookiesDistribution[i]+=cookies[idx];
-//             dfs(cookies,k,cookiesDistribution,idx-1);
-//             //undo step
-//             cookiesDistribution[i]-=cookies[idx];
-//         }
-//     }
-// public:
-    // int distributeCookies(vector<int>& cookies, int k) {
-    //     vector<int> cookiesDistribution(k);
-    //     dfs(cookies,k,cookiesDistribution, cookies.size()-1);
-    //     return minUnfairness;
-    // }
+            //Perform tasks
+            int largeAns=0;
+            for(int i=0;i<k;i++){
+                int curChildCookieCnt=cookiesDistribution[i];
+                largeAns=max( {largeAns,curChildCookieCnt} );
+            }
+            // cout<<largeAns<<endl;
+            minUnfairness=min({minUnfairness,largeAns});
+            return;
+        }
+        for(int i=0;i<k;i++){
+            cookiesDistribution[i]+=cookies[idx];
+            dfs(cookies,k,cookiesDistribution,idx + 1);
+            //undo step
+            cookiesDistribution[i]-=cookies[idx];
+            if(!cookiesDistribution[i])
+                break;
+        }
+    }
+public:
+    int distributeCookies(vector<int>& cookies, int k) {
+        vector<int> cookiesDistribution(10);
+        dfs(cookies,k,cookiesDistribution, 0);
+        return minUnfairness;
+    }
     
     /*
     
@@ -39,36 +42,36 @@ class Solution {
     8
     
     */
-public:
+// public:
      
-   int maxi=INT_MAX;
-    void explore(vector<int>&cookies,int n,int k,vector<int>&vec)
-    {
-      if(n==cookies.size())
-      {
-          int m=0;
-          for(int i=0;i<k;i++)
-          {
-              m=max(m,vec[i]);
+//    int maxi=INT_MAX;
+//     void explore(vector<int>&cookies,int n,int k,vector<int>&vec)
+//     {
+//       if(n==cookies.size())
+//       {
+//           int m=0;
+//           for(int i=0;i<k;i++)
+//           {
+//               m=max(m,vec[i]);
               
-          }
-          maxi=min(maxi,m);
-          return;
-      }
-        for(int i=0;i<k;i++)
-        {
-            vec[i]+=cookies[n];
-            explore(cookies,n+1,k,vec);
-            vec[i]-=cookies[n];
-        }
-    }
+//           }
+//           maxi=min(maxi,m);
+//           return;
+//       }
+//         for(int i=0;i<k;i++)
+//         {
+//             vec[i]+=cookies[n];
+//             explore(cookies,n+1,k,vec);
+//             vec[i]-=cookies[n];
+//         }
+//     }
     
        
-    int distributeCookies(vector<int>& cookies, int k) {
+//     int distributeCookies(vector<int>& cookies, int k) {
       
-        vector<int> vec(10,0);
-          explore(cookies,0,k,vec); 
-        return maxi;
+//         vector<int> vec(10,0);
+//           explore(cookies,0,k,vec); 
+//         return maxi;
         
-    }
+//      }
 };
