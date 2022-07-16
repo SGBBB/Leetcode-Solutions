b07cb0fd83b080cc4b8c8c84a7e42e4429016a84
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-07-16 21:35:15 +0530

    Time: 63 ms (24.52%), Space: 27.4 MB (44.85%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -2,62 +2,53 @@ class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
         /*
+        2 6 4 8 10 9 15
         
-    2,(6,4,8,10,9),15  
-    
-    Observations:
-    At worst c ase I will have to consider whole length
-    
-    1,2,3,4
+        must start from idx 2
+        we must consider extreme left anomaly point and extreme 
         
-    if previous guy is greater than current guy then anomaly started
-    Hence, previous guy is gonna participate in our short-subarray.
-    
-    
-    Do I need to just find the start and end points
-    1 4 2 13 9 25   
+        0 1 1 1 -1 2 4 5  3 6 7
         
-    2 6 4   
+        1,2,3,4
+        
+        4 ,3, 2 ,1
+        
+        stack
+        2
+        
+        start=0
+        end=n-1 =3
         */
-        
-        int n=nums.size(),maxNumUntilNow=INT_MIN ;
-        int startIdx=n,endIdx=-1,visitOnce=0;
-        
-        /*
-        
-        Anyway my iteration has to be linear anyhow 
-        Taks is to find the last anomalous point
-        
-        */
-        
-        stack<int> st;// stack is holding indices
-        for(int i=0;i<n;i++){
+        stack<int> st;
+        int len=nums.size();
+        int start=len,end=-1;
+        if(len<2)
+            return 0;
+         
+        //computing start idx
+        for(int i=0;i<len;i++){
             while(!st.empty() && nums[i]<nums[st.top()]){
-                //I wanna choose extremely smaller left anomaly index
-                startIdx=min({startIdx,st.top()}),
+                start=min({start,st.top()});st.pop();
+            }
+            st.push(i);
+        }
+        if(start==len)
+            return 0;
+        while(!st.empty())
+            st.pop();
+        //computing the extremee right idx
+        for(int i=len-1;i>-1;i--){
+            while(!st.empty() && nums[i]>nums[st.top()]){
+                end=max({st.top(),end});
                 st.pop();
             }
             st.push(i);
         }
+        return end-start+1;
+        /*
         
         
-        //if startIdx isnt changed then Im sure that array is arranged in ascending order
-        if(startIdx==n)
-            return 0;
-        // Making stack empty for next iteration
-        while(!st.empty())
-            st.pop();
-       for(int i=n-1;i>=0;i--){
-            while(!st.empty() && nums[i]>nums[st.top()]){
-                //I wanna choose extreme right index where anomaly occured
-                endIdx=max({endIdx,st.top()});
-                st.pop();
-            }
-           st.push(i);
-       }
-        
-       return endIdx-startIdx+1;
-        
+        */
         
     }
 };
