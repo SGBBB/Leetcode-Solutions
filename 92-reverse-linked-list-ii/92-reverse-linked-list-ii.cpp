17fd2c2a4dcf362d5998b3815ad966a4991e48e1
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-07-21 22:35:17 +0530

    Time: 3 ms (72.05%), Space: 7.8 MB (17.76%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
@@ -9,47 +9,34 @@
  * };
  */
 class Solution {
-    vector<ListNode*> reverse(ListNode * node,int left,int right){
-    //If i  dont appply recursion then it can be achieved iteratively as well
-    cout<<left<<right<<endl;
-    ListNode* prevNode=NULL;
-    while( left<right ){
-        
-        ListNode* nextNode=node->next;
-        node->next=prevNode;
-        prevNode=node;
-        node=nextNode;
-        left++;
+    //blackbox work to reverse linked list
+    vector<ListNode*> reverseList(ListNode* node,int left, int right){
+        vector<ListNode*> arr(2);
+        if(left<right){
+            vector<ListNode*> response=reverseList(node->next,left+1,right);
+            node->next->next=node;
+            node->next=NULL;
+            arr[0]=response[0],arr[1]=response[1];
+        }
+        else if(left==right)
+            arr[0]=node,arr[1]=node->next;
+        return arr;
     }
-    cout<<"node val is  "<<node->val <<endl;
-    bool nextNodeNull=0;
-    if( node->next ){
-        ListNode* nextNode=node->next;
-        node->next=prevNode; 
-        return {node,nextNode};
-    }
-        //Now node-.next is NULL
-    
-    node->next=prevNode; 
-    return {node,NULL};
-    
- }
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        ListNode* dummyNode=new ListNode(-1), *prevNode;
+        int pos=0;
+        ListNode* dummyNode=new ListNode(-1);
         dummyNode->next=head;
-        prevNode=dummyNode;
-        
-    int cnt=0; 
-    while(cnt+1<left){
-        cout<<prevNode->next->val <<endl;
-        prevNode=prevNode->next;
-        cnt++;
-    }
-    ListNode* nextNode=prevNode->next;
-    vector<ListNode*> NodeArray=reverse(prevNode->next,left,right);
-    prevNode->next=NodeArray[0],
-    nextNode->next=NodeArray[1];
-    return dummyNode->next;
+        ListNode* curNode=dummyNode;
+        while(pos+1<left)
+            curNode=curNode->next,pos++;
+        ListNode* nextNode=curNode->next;
+        vector<ListNode*>response= reverseList(nextNode,left,right);
+        curNode->next=response[0];
+        //if List after newHead exists then 
+        if(response[1]){
+            nextNode->next=response[1];
+        }
+        return dummyNode->next;
     }
 };
