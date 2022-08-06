d00e8499a3fbb59e953080617788a8c006f746f4
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-08-06 08:09:06 +0530

    Time: 2 ms (67.26%), Space: 6.5 MB (91.95%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -7,18 +7,20 @@ public:
         return combinationSum4Helper(nums,target  );
     }
     int combinationSum4Helper(vector<int>& nums, int target) {
-        if(target<=0){
-            if(target==0) 
-                return 1;
-            return 0;
-        }
+        //if target is 0 then return 0;
+        if(target==0) 
+            return 1;
+            
+        //If we have already found target then return the saved answer
+        
         if(dp[target]!=-1)
             return dp[target];
         int len=nums.size(),
         totalCombinations=0;
         
             for(int i=0;i<len;i++)
-                totalCombinations+=combinationSum4Helper( nums,target-nums[i] );
+                if(nums[i]<=target)
+                    totalCombinations+=combinationSum4Helper( nums,target-nums[i] );
         
         return dp[target]=totalCombinations;
     }
