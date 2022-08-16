6e8b3da3d4150c753ee50cf459b70737cbbaadca
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-08-16 20:12:49 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (21.58%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -4,57 +4,33 @@ public:
         /*
         
         1,17,5,10,13,15,10,5,16,8
-        1 2  2 4  4  5  5  5 7  7   
-        1 1  3 3  4  4  4  6 6  8
+        
         */
-        int size=nums.size(), f=1, d=1;
-        for(int i=1; i<size; ++i){
-                 if(nums[i]>nums[i-1]) f=d+1;
-                 else if(nums[i]<nums[i-1]) d=f+1;
-        }
-        return max( {f, d} );
-    }
-    int wiggleMaxLength1(vector<int>& nums) {
-        /*
-        hint mark these points on a bar graph 
-        problem is solved
-        1,7,4,9,2,5
         
-        
-        1,17,5,10,13,15,10,5,16,8
-        1 17 5 10 5 16 8
-        
-        
-        when encountering the new guy update cur guy with highest               possible height until now
-        
-        
-        Try some underlying test cases :
-        15 12  8 31 13 11 18 4 444  12
-        
-        
-        
-         
-         
-        */
-        int n=nums.size();
-        int maxLen=0;
-        int prev_direction=-9000000; //arbitrary value
-        for(int i=1;i<n;i++){
-            int direction=nums[i]-nums[i-1];
-            direction=(!direction)?0: (direction<0) ? -1 :1 ;
-            // cout<<direction<<endl;
-            if(  !direction || direction==prev_direction ){
-                //dont update maxLen  
-                continue;
+        int n=nums.size(),i=1,cnt=0,prev=nums[0], slopeNature=0;
+        if(n<2)
+            return n;
+        while(i<n){
+            if(!slopeNature){
+                if(nums[i]>nums[i-1])
+                    cnt++,slopeNature=1,prev=nums[i];
+                else if(nums[i]<nums[i-1])
+                    cnt++,slopeNature=-1,prev=nums[i];
+//          
             }
-            else{ // found an alternating pair
-                //update maxLen
-                maxLen++,
-                //update prev_direction
-                prev_direction=direction;
+            else if(slopeNature<0){
+                if(nums[i]>nums[i-1]){
+                    cnt++,slopeNature=1;
+                }
             }
+//        if last     slopeNature is +ve
+            else{
+                if(nums[i]<nums[i-1]){
+                    cnt++,slopeNature=-1;
+                }
+            }
+            i++;
         }
-        // for(int i=1;;)
-        return maxLen+1;
+        return cnt+1;
     }
 };
