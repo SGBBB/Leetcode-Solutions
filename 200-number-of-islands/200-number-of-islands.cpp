9133f683878eab56408f75f2a3cccbb3b81b94ec
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-08-29 07:45:25 +0530

    Time: 49 ms (64.88%), Space: 12.3 MB (83.10%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    void eraseIslands(vector<vector<char>>& grid,int i,int j){
+        int dir[]={-1,0,1,0,-1};//URDL
+        //make itself 0 
+            grid[i][j]='0';
+        int r=grid.size(),c=grid[0].size();
+        //travel to 4 directions
+        for(int k=0;k<4;k++){
+            
+            int newi=i+dir[k],newj=j+dir[k+1];
+            //ensuring whether coming cell is under boundry and is 1
+            if(min(newi,newj)>-1 && newi<r && newj<c && grid[newi][newj]=='1'){
+                eraseIslands(grid,newi,newj);
+            }
+        }
+    }
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size(), n = m ? grid[0].size() : 0, islands = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    islands++;
+                    eraseIslands(grid, i, j);
+                }
+            }
+        }
+        return islands;
+         
+    }
+};
