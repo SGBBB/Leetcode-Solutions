2864d6807597774d0855dc5127b51306bb290881
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2022-08-31 23:48:14 +0530

    Time: 382 ms (11.33%), Space: 72.8 MB (27.50%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -29,42 +29,5 @@ public:
     }
 };
 /*
-How will i go ahead solving this problem???
-.Observatoiins:
-->return maximum no. of operations
-->if k is smaller than min num of the array then retunr 0;
-[1 ,1 ,1 ,1  ,2 ,3 ,4  ,2]
-
-k(4)
-
-Ans->2
-
-
-
-
-(Q) is maximum no of operations equivalent to total opoerations ???
-
-
-
-1  1  1  1   2  3  4   5  2  2 3 3 6 
-
-k(7)
-
-1  1  1  1   2 2  2 3 3 3  4   5   6 
-
-cnt=2
-
-Ok its just application of 2 sum
-
-
-
-5 ,  6,8,19,21,21,21
-
-Should i think more?????????????????????
-
-5 ,  6,8,8,9,12,12,12
-k(11)
-
-
-Time complexity of this solution is nlogn
+Time & Space: O(N)
 */
